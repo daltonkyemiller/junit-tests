3573ad7233d48eaeef576ffeb4ad236a87588d08
Dalton Miller <dalton.kye.miller@gmail.com>
2022-07-05 10:27:13 -0500

    finished Student Tests

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -5,32 +6,33 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class StudentTest {
+    private Student student;
+    @Before
+    public void setUp() {
+        student = new Student("Dalton", 5);
+
+        student.addGrade(100);
+        student.addGrade(90);
+        student.addGrade(50);
+    }
 
     @Test
     public void testConstructor() {
-        Student student = new Student("Dalton", 5);
 
         assertEquals("Dalton", student.getName());
         assertEquals(5, student.getId());
-        assertEquals(new ArrayList<>(), student.getGrades());
+        assertEquals(3, student.getGrades().size());
     }
 
     @Test
     public void testAddGrade() {
-        Student student = new Student("Dalton", 5);
-
-        student.addGrade(100);
-        assertEquals(1, student.getGrades().size());
+        assertEquals(3, student.getGrades().size());
         assertEquals(100, student.getGrades().get(0).intValue());
     }
 
     @Test
     public void testGradeAverage() {
-        Student student = new Student("Dalton", 5);
 
-        student.addGrade(100);
-        student.addGrade(90);
-        student.addGrade(50);
         assertEquals(80, student.getGradeAverage(), 0.01);
     }
 
